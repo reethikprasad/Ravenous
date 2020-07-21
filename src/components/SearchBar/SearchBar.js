99b5169fac0cf4ee43f2c6c993a9610367c90109
Reethik Prasad <reethikprasad@gmail.com>
2020-07-21 18:21:08 +0530

    image not loading

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './SearchBar.css';
 
+
 const sortByOptions={
     'Best Match':'best_match' ,
     'Highest Rated':'rating' ,
@@ -14,4 +15,28 @@ class SearchBar extends React.Component{
             return <li key={sortByOptionValue}>{sortByOption}</li>
         });
     }
+
+
+render(){
+      return (
+            <div className="SearchBar">
+          <div className="SearchBar-sort-options">
+            <ul>
+              {this.renderSortByOptions()}
+            </ul>
+          </div>
+          <div className="SearchBar-fields">
+            <input placeholder="Search Businesses" />
+            <input placeholder="Where?" />
+          </div>
+          <div className="SearchBar-submit">
+            <a href="www.#.com">Let's Go</a>
+          </div>
+        </div>
+      )
+    }
 }
+
+
+
+export default SearchBar;
